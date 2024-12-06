41776192112ba3267343b0bcd3aff93e651bc8af
CVS-otherwise-Callie <notes2noah@gmail.com>
2024-12-06 08:32:17 -0500

    Create script loader.lua

---
+++ scripts/script loader.lua
@@ -0,0 +1,26 @@
+function FHAC:LoadScripts(includestart, t)
+    for k, v in ipairs(t) do
+        if includestart then v= includestart.."." .. v end
+        include(v)
+    end
+end
+
+if StageAPI and StageAPI.Loaded then
+StageAPI.UnregisterCallbacks("FHAC")
+FHAC:LoadScripts("scripts", {
+	"savedata",
+	"constants",
+	"iam.main",
+	"callbacks",
+	"iam.misc.resources.fortunes",
+	"library",
+	"items",
+	"entities2",
+	"compatability.fiend folio.modcompact",
+	"otherapi.fiend folio.api.fortunehandling",
+	"otherapi.fiend folio.api.apioverride",
+	"otherapi.proapi.proapi",
+})
+else
+	include("scripts.iam.misc.warning")
+end
