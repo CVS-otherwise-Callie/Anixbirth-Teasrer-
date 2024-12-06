d8aaee2efb36423fa005d8b6d426b268f7240011
CVS-otherwise-Callie <notes2noah@gmail.com>
2024-12-06 08:33:17 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,30 +1,4 @@
 FHAC = RegisterMod("Fivehead", 1)
 
-function FHAC:LoadScripts(includestart, t)
-    for k, v in ipairs(t) do
-        if includestart then v= includestart.."." .. v end
-        include(v)
-    end
-end
-
-if StageAPI and StageAPI.Loaded then
-StageAPI.UnregisterCallbacks("FHAC")
-FHAC:LoadScripts("scripts", {
-	"savedata",
-	"constants",
-	"iam.main",
-	"callbacks",
-	"iam.misc.resources.fortunes",
-	"library",
-	"items",
-	"entities2",
-	"compatability.fiend folio.modcompact",
-	"otherapi.fiend folio.api.fortunehandling",
-	"otherapi.fiend folio.api.apioverride",
-	"otherapi.proapi.proapi",
-})
---ff
-else
-	include("scripts.iam.misc.warning")
-end
+include("scripts.script loader")
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
