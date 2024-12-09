885fb0746a5666dacb0a1ebaf637c870a8bc8479
CVS-otherwise-Callie <notes2noah@gmail.com>
2024-12-09 10:15:06 -0500

    Update joke book.lua

--- scripts/iam/items/actives/joke book.lua
+++ scripts/iam/items/actives/joke book.lua
@@ -1,7 +1,16 @@
 local mod = FHAC
 
 function mod:JokeBook(item, _, player)
+    local coolnumber = mod.DSSavedata.fortuneLanguage
     mod.JokeBookDelay = true
+    mod.DSSavedata.fortuneLanguage = 4
+    mod.FortuneTable = {}
+    mod:ShowFortune()
+    mod.DSSavedata.fortuneLanguage = coolnumber
+    mod.FortuneTable = {}
+
+
+    
     for k, v in ipairs(Isaac.GetRoomEntities()) do
         if v:IsActiveEnemy() then
             v:AddFear(EntityRef(player), 150)
