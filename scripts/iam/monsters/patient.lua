03384b015cc98bfec9732ac189f3af1c2a68116b
CVS-otherwise-Callie <notes2noah@gmail.com>
2025-01-01 12:32:02 -0500

    set up tables for patient types (still need to animate/code)

--- scripts/iam/monsters/patient.lua
+++ scripts/iam/monsters/patient.lua
@@ -18,14 +18,120 @@ function mod:PatientAI(npc, sprite, d)
     local projparams = ProjectileParams()
     local patienttypes = {
         {
-            name = "Virus",
+            name = "virus",
             speed = "1",
+            size = "1",
             behavior = "WanderShoot",
+            detectrange = "230",
             shootoffset = "60",
+            shottypes = {"Blood"},
+            shotdamage = 1,
             headStart = math.random(1, 10),
             wait = math.random(-5, 5),
-            state = "Moving"
-        }
+            state = "Moving",
+            subtype = "0"
+        },
+        {
+            name = "speed",
+            speed = "2",
+            size = "1",
+            behavior = "Chase",
+            subbehavior = "ProAPI",
+            shootoffset = "0",
+            headStart = math.random(1, 10),
+            wait = math.random(-5, 5),
+            state = "Moving",
+            subtype = "1"
+        },
+        {
+            name = "roid",
+            speed = "0.65",
+            size = "1",
+            behavior = "WanderShoot",
+            detectrange = "170",
+            subbehavior = "Charge",
+            shottypes = {"Homing"},
+            shotdamage = 1,
+            shootoffset = "100",
+            headStart = math.random(1, 10),
+            wait = math.random(-5, 10),
+            state = "Moving",
+            subtype = "2"
+        },
+        {
+            name = "experimental",
+            speed = "1.7",
+            size = "1",
+            behavior = "WanderShoot",
+            detectrange = "210",
+            subbehavior = "ProAPI",
+            shottypes = {"Inner Eye", "Poly", "Soy Milk", "Number One"},
+            shotdamage = 1, --this is a placeholder for ex. since it's based on chosen shot type
+            shootoffset = math.random(40, 70),
+            headStart = math.random(1, 10),
+            wait = math.random(-5, 5),
+            state = "Moving",
+            subtype = "3"
+        },
+        {
+            name = "growth",
+            speed = "1.3",
+            size = "1.5",
+            behavior = "WanderShoot",
+            detectrange = "250",
+            shootoffset = "40",
+            shottypes = {"Blood"},
+            shotdamage = 2,
+            headStart = math.random(1, 10),
+            wait = math.random(-5, 5),
+            state = "Moving",
+            subtype = "4"
+        },
+        {
+            name = "synthoil",
+            speed = "0.8",
+            size = "1",
+            behavior = "WanderShoot",
+            subbehavior = "Neutral",
+            detectrange = "100000",
+            shootoffset = "70",
+            shottypes = {"Blood"},
+            shotdamage = 1,
+            headStart = math.random(1, 10),
+            wait = math.random(-5, 5),
+            state = "Moving",
+            subtype = "5"
+        },
+        {
+            name = "adrenaline",
+            speed = "1",
+            size = "1",
+            behavior = "WanderShoot",
+            subbehavior = "HealSpeed",
+            gainedspeed = "0.1",
+            detectrange = "230",
+            shootoffset = "60",
+            shottypes = {"Blood"},
+            shotdamage = 1,
+            headStart = math.random(1, 10),
+            wait = math.random(-5, 5),
+            state = "Moving",
+            subtype = "6"
+        },
+        {
+            name = "euthanasia",
+            speed = "1",
+            size = "1",
+            behavior = "WanderShoot",
+            detectrange = "230",
+            shootoffset = "60",
+            shottypes = {"Euthanasia"},
+            shotdamage = 1,
+            headStart = math.random(1, 10),
+            wait = math.random(-5, 5),
+            state = "Moving",
+            subtype = "7"
+        },
     }
 
     if not d.init then
