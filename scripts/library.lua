fb5c168eba0d09a927b9d3155e9b0574c55e0390
CVS-otherwise-Callie <notes2noah@gmail.com>
2025-04-03 12:23:36 -0400

    Update library.lua

--- scripts/library.lua
+++ scripts/library.lua
@@ -16,6 +16,16 @@ function mod:getMinSec(totalSeconds)
     return minutes, seconds
 end
 
+function mod:AnyPlayerHasCollectible(coll)
+	for i = 1, game:GetNumPlayers() do
+        if game:GetPlayer(i):ToPlayer():HasCollectible(coll) then
+            return true
+        end
+    end
+
+    return false
+end
+
 function mod:GetCirc(rad, per)
 	return Vector(rad * math.sin(per), rad * math.cos(per))
 end
