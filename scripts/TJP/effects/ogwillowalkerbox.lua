c48d568a8e8b9cee0ba033ba4c50458e5700b672
CVS-otherwise-Callie <notes2noah@gmail.com>
2025-06-25 19:56:45 -0400

    im so done

--- scripts/TJP/effects/ogwillowalkerbox.lua
+++ scripts/TJP/effects/ogwillowalkerbox.lua
@@ -2,39 +2,36 @@ local mod = FHAC
 local sfx = SFXManager()
 local game = Game()
 
-mod:AddCallback(ModCallbacks.MC_POST_EFFECT_UPDATE, function(_, effect)
-    if effect.Variant == mod.Effects.OGWilloWalkerBox.Var then
-        if not Options.Fullscreen then
-            effect.Position = Isaac.ScreenToWorld(Vector(Isaac.GetScreenWidth(), (Isaac.GetScreenHeight())))
-        else
-            effect.Position = Isaac.ScreenToWorld(Vector(Isaac.GetScreenWidth(), (Isaac.GetScreenHeight())))
+mod:AddCallback(ModCallbacks.MC_POST_RENDER, function()
+
+	local center = StageAPI.GetScreenCenterPosition()
+	local bottomright = StageAPI.GetScreenBottomRight()
+	local bcenter = Vector(center.X, bottomright.Y - 300)
+
+    if #Isaac.FindByType(mod.Effects.OGWilloWalkerBox.ID, mod.Effects.OGWilloWalkerBox.Var) ~= 0 then
+        local tab = Isaac.FindByType(mod.Effects.OGWilloWalkerBox.ID, mod.Effects.OGWilloWalkerBox.Var)
+        local ef = tab[#tab]
+        if #tab > 1 then -- no overflows
+            for i = 1, #tab - 1 do
+                tab[i]:Remove()
+            end
         end
-        mod:OGWilloWalkerBoxAI(effect, effect:GetSprite(), effect:GetData())
+
+        ef.Position = Vector(bcenter.X, Isaac.GetScreenHeight() - 35)
+
+        ef.Visible = false
+
+        local sprite = ef:GetSprite()
+        local d = ef:GetData()
+
+        sprite:Render(Vector(bcenter.X, Isaac.GetScreenHeight() - 35), Vector.Zero, Vector.Zero)
+
+        if not ef.Child then
+            d.font = Isaac.Spawn(1000, 430, 55, ef.Position, ef.Velocity, ef):ToEffect() --Isaac.ScreenToWorld(Vector(Isaac.GetScreenWidth(), (Isaac.GetScreenHeight()*2)-70))
+            d.font.Parent = ef
+            ef.Child = d.font
+            d.font = ef.Child:GetData()
+        end
+        ef.DepthOffset = 500
     end
 end)
-
-mod:AddCallback(ModCallbacks.MC_POST_EFFECT_RENDER, function(_, effect)
-    if not Options.Fullscreen then
-        effect.Position = Isaac.ScreenToWorld(Vector(Isaac.GetScreenWidth(), (Isaac.GetScreenHeight())))
-    else
-        effect.Position = Isaac.ScreenToWorld(Vector(Isaac.GetScreenWidth(), (Isaac.GetScreenHeight())))
-    end
-    --print(Isaac.GetScreenWidth())
-    --effect.Position = Isaac.ScreenToWorld(Vector(Isaac.GetScreenWidth(), (Isaac.GetScreenHeight())))
-end, mod.Effects.OGWilloWalkerBox.Var)
-
-function mod:OGWilloWalkerBoxAI(ef, sprite, d)
-
-    if not d.init then
-        d.init = true
-    end
-
-    if not ef.Child then
-        d.font = Isaac.Spawn(1000, 430, 55, ef.Position, ef.Velocity, ef):ToEffect() --Isaac.ScreenToWorld(Vector(Isaac.GetScreenWidth(), (Isaac.GetScreenHeight()*2)-70))
-        d.font.Parent = ef
-        ef.Child = d.font
-        d.font = ef.Child:GetData()
-    end
-    print(Options.Fullscreen)
-    ef.DepthOffset = 500
-end
--- scripts/TJP/effects/ogwillowalkerfont.lua
+++ scripts/TJP/effects/ogwillowalkerfont.lua
@@ -3,70 +3,63 @@ local sfx = SFXManager()
 local game = Game()
 local room = game:GetRoom()
 
-mod:AddCallback(ModCallbacks.MC_POST_EFFECT_UPDATE, function(_, effect)
-    if effect.Variant == mod.Effects.OGWilloWalkerFont.Var then
-        mod:OGWilloWalkerFontAI(effect, effect:GetSprite(), effect:GetData())
-    end
-end)
-
 mod:AddCallback(ModCallbacks.MC_POST_RENDER, function()
 
-    for k, v in ipairs(Isaac.GetRoomEntities()) do
-        if v.Type == 1000 and v.Variant == mod.Effects.OGWilloWalkerFont.Var then
-            local ef = v
-            local sprite = v:GetSprite()
-            local d = v:GetData()
+	local center = StageAPI.GetScreenCenterPosition()
+	local bottomright = StageAPI.GetScreenBottomRight()
+	local bcenter = Vector(center.X, bottomright.Y - 300)
 
-            local function textToAsciiNumberConverter(char)
-                local ascii = string.byte(char)
-
-                return ascii - 32
+    if #Isaac.FindByType(mod.Effects.OGWilloWalkerFont.ID, mod.Effects.OGWilloWalkerFont.Var) ~= 0 then
+        local tab = Isaac.FindByType(mod.Effects.OGWilloWalkerFont.ID, mod.Effects.OGWilloWalkerFont.Var)
+        local ef = tab[#tab]
+        if #tab > 1 then -- no overflows
+            for i = 1, #tab - 1 do
+                tab[i]:Remove()
             end
-            local function changeTextColour(ef, colour)
-                if colour == "W" then
-                    ef:SetColor(Color.Default, 2, 1, false, false)
-                    --return "W (ts in func)"
-                end
-                if colour == "Y" then
-                    ef:SetColor(Color(1,1,0,1), 2, 1, false, false)
-                    --return "Y (ts in func)"
-                end
-            end
-
-            d.sent =  [[These \Ywillos \Ware \YPissing \Wme off...]]
-
-            d.loopcount = 0
-            if ef.Parent then
-                ef.Position = room:WorldToScreenPosition(ef.Parent.Position) - Vector(230, -120)
-
-            else
-                ef.Position = Vector(100,100)
-            end
-            changeTextColour(ef, "W")
-            ef.Visible = true
-            while d.loopcount < #d.sent do
-                d.loopcount = d.loopcount + 1
-                local letter = string.sub(d.sent,d.loopcount,d.loopcount)
-                local ascii = textToAsciiNumberConverter(letter)
-                if string.sub(d.sent,d.loopcount,d.loopcount) == [[\]] then
-                    local colour = string.sub(d.sent,d.loopcount + 1,d.loopcount + 1)
-                    changeTextColour(ef, colour)
-                    d.loopcount = d.loopcount + 1
-                else
-                    sprite:SetFrame(ascii)
-                    ef:Render(Vector(0,0))
-                    ef.Position = ef.Position + Vector(11,0)
-                end
-            end
-            ef.Visible = false
         end
+
+        local sprite = ef:GetSprite()
+        local d = ef:GetData()
+
+        local function textToAsciiNumberConverter(char)
+            local ascii = string.byte(char)
+
+            return ascii - 32
+        end
+        local function changeTextColour(ef, colour)
+            if colour == "W" then
+                ef:SetColor(Color.Default, 2, 1, false, false)
+                --return "W (ts in func)"
+            end
+            if colour == "Y" then
+                ef:SetColor(Color(1,1,0,1), 2, 1, false, false)
+                --return "Y (ts in func)"
+            end
+        end
+
+        d.sent =  [[These \Ywillos \Ware \YPissing \Wme off...]]
+
+        d.loopcount = 0
+        if not ef.Parent then
+            ef:Remove()
+        else
+            ef.Position = ef.Parent.Position
+        end
+        changeTextColour(ef, "W")
+        ef.Visible = true
+        while d.loopcount < #d.sent do
+            d.loopcount = d.loopcount + 1
+            local letter = string.sub(d.sent,d.loopcount,d.loopcount)
+            local ascii = textToAsciiNumberConverter(letter)
+            if string.sub(d.sent,d.loopcount,d.loopcount) == [[\]] then
+                local colour = string.sub(d.sent,d.loopcount + 1,d.loopcount + 1)
+                changeTextColour(ef, colour)
+                d.loopcount = d.loopcount + 1
+            else
+                sprite:SetFrame(ascii)
+                sprite:Render(Vector(bcenter.X + (d.loopcount * 8) - 200, Isaac.GetScreenHeight() - 35), Vector.Zero, Vector.Zero)
+            end
+        end
+        ef.Visible = false
     end
 end)
-
-function mod:OGWilloWalkerFontAI(ef, sprite, d)
-
-    if not d.init then
-        d.init = true
-    end
-
-end
